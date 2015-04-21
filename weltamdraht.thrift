851c2da3b474e7c9d96fddaf8fc835f65db2de1b
Bret Curtis <psi29a@gmail.com>
2015-04-21 23:20:37 +0200

    Spatial location get and set

--- weltamdraht.thrift
+++ weltamdraht.thrift
@@ -9,6 +9,12 @@ struct WAD_Signature {
     3: i16 hmac;
 }
 
+struct WAD_Spatial_Location {
+    1: i16 x;
+    2: i16 y;
+    3: i16 z;
+}
+
 service WeltamDraht{
     /**
      * Ping call - returns server version
@@ -16,5 +22,19 @@ service WeltamDraht{
     string ping(
         1: WAD_Signature signature,
         2: string client_version
+    )
+
+    /**
+     * Get Spatial Field Location - returns Location
+     */
+    WAD_Spatial_Location getSpatialLocation(
+        1: WAD_Signature signature
+    )
+
+    /**
+     * Set Spatial Field Location - returns Bool
+     */
+    bool setSpatialLocation(
+        1: WAD_Signature signature
     )
 }
